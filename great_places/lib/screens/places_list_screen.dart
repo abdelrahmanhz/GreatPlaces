74b2278b1e5f9a5267797e28facb781e7e507f6f
Abdelrahman Hamza <64665314+abdelrahmanhz@users.noreply.github.com>
2022-08-17 16:12:32 +0200

    Initial commit

---
+++ great_places/lib/screens/places_list_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/screens/add_place_screen.dart';
+
+class PlacesListScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [
+          IconButton(onPressed: () {
+            Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+          }, icon: const Icon(Icons.add),)
+        ],
+      ),
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
